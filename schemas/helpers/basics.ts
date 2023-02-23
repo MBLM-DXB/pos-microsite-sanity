4f30490f0a56ae25a07c9a3f5dae316761e55cc5
bruno teixeira <btex88@gmail.com>
2023-02-23 17:02:30 -0300

    fix: change page content name to lowercase

--- schemas/helpers/basics.ts
+++ schemas/helpers/basics.ts
@@ -70,7 +70,7 @@ export const pageBasicInfo = () => {
             ]
         },
         {
-            name: 'Content',
+            name: 'content',
             type: 'array',
             title: 'Content',
             of: [
